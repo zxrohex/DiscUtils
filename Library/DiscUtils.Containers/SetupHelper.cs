7869bcb8972ae95ab805f54a69b46c8572ab1bf8
Olof Lagerkvist <ol@ltr-data.se>
2023-01-07 00:48:26 +0100

    Added lvm support to DiscUtils.Containers

--- Library/DiscUtils.Containers/SetupHelper.cs
+++ Library/DiscUtils.Containers/SetupHelper.cs
@@ -1,20 +1,17 @@
-﻿using DiscUtils.Dmg;
-using DiscUtils.Iso9660;
-using DiscUtils.Wim;
-
-namespace DiscUtils.Containers;
+﻿namespace DiscUtils.Containers;
 
 public static class SetupHelper
 {
     public static void SetupContainers()
     {
-        Setup.SetupHelper.RegisterAssembly(typeof(Disk).Assembly);
-        Setup.SetupHelper.RegisterAssembly(typeof(BuildFileInfo).Assembly);
+        Setup.SetupHelper.RegisterAssembly(typeof(Dmg.Disk).Assembly);
+        Setup.SetupHelper.RegisterAssembly(typeof(Iso9660.BuildFileInfo).Assembly);
+        Setup.SetupHelper.RegisterAssembly(typeof(Lvm.LogicalVolumeManager).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Vhd.Disk).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Vhdx.Disk).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Vmdk.Disk).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Vdi.Disk).Assembly);
-        Setup.SetupHelper.RegisterAssembly(typeof(WimFile).Assembly);
+        Setup.SetupHelper.RegisterAssembly(typeof(Wim.WimFile).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Xva.Disk).Assembly);
     }
 }
