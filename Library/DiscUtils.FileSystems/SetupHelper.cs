60530d14a73a459306e940f5f8d0198d89f64023
Olof Lagerkvist <ol@ltr-data.se>
2023-01-07 00:27:10 +0100

    Added btrfs and swap to DiscUtils.FileSystems

--- Library/DiscUtils.FileSystems/SetupHelper.cs
+++ Library/DiscUtils.FileSystems/SetupHelper.cs
@@ -1,9 +1,11 @@
-﻿using DiscUtils.Ext;
+﻿using DiscUtils.Btrfs;
+using DiscUtils.Ext;
 using DiscUtils.Fat;
 using DiscUtils.HfsPlus;
 using DiscUtils.Ntfs;
 using DiscUtils.OpticalDisk;
 using DiscUtils.SquashFs;
+using DiscUtils.Swap;
 using DiscUtils.Xfs;
 
 namespace DiscUtils.FileSystems;
@@ -12,12 +14,14 @@ public static class SetupHelper
 {
     public static void SetupFileSystems()
     {
+        Setup.SetupHelper.RegisterAssembly(typeof(BtrfsFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(ExtFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(FatFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(HfsPlusFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(NtfsFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(Disc).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(SquashFileSystemBuilder).Assembly);
+        Setup.SetupHelper.RegisterAssembly(typeof(SwapFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(XfsFileSystem).Assembly);
         Setup.SetupHelper.RegisterAssembly(typeof(VirtualFileSystem.VirtualFileSystem).Assembly);
     }
